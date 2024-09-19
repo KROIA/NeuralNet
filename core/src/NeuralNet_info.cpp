627b89594780bb1660601fe84f3cf78f285a2df6
Alex Krieg <alexkrieg@gmx.ch>
2024-09-19 15:25:14 +0200

    ~ Update Library Template 1.3.0

--- core/src/NeuralNet_info.cpp
+++ core/src/NeuralNet_info.cpp
@@ -1,5 +1,4 @@
 #include "NeuralNet_info.h"
-#include "NeuralNet_debug.h"
 
 /// USER_SECTION_START 1
 
@@ -151,25 +150,7 @@ namespace NeuralNet
 	}
 #endif
 
-	// Implementation of the Profiler start/stop functions
-	void Profiler::start()
-	{
-#ifdef NN_PROFILING
-		EASY_PROFILER_ENABLE;
-#endif
-	}
-	void Profiler::stop()
-	{
-		stop("profile.prof");
-	}
-	void Profiler::stop(const char* profilerOutputFile)
-	{
-#ifdef NN_PROFILING
-		profiler::dumpBlocksToFile(profilerOutputFile);
-#else
-		(void)profilerOutputFile;
-#endif
-	}
+
 
 
 /// USER_SECTION_START 4
